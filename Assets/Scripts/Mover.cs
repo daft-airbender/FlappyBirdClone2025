b14cf305b4b473fdc96ad7d612801dc5dc9592b6
Edward Evangelista <daftairbendr@gmail.com>
2025-12-05 19:39:47 -0500

    Establish basic 'move to the right' logic for Mover.cs script

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,15 +2,11 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.right * speed * Time.deltaTime;
     }
 }
